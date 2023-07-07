2b748be1453b92495958b1895505fc14c4e30083
dio-p <dionempapadop@gmail.com>
2023-07-07 16:25:55 +0100

    the child seems to be returned correctly

--- src/components /EntityComp.js
+++ src/components /EntityComp.js
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
 import styled from "@emotion/styled";
 import MultiBtnComp from "./MultiBtnComp";
+import useChildrenByIdsSearch from "../hooks/queries/useChildrenByIdsSearch";
 
 const EntityContainer = styled.div`
   display: flex;
 `;
 
 const EntityComp = ({ entity }) => {
+
+  const [returnedChildren, searchChildren] = useChildrenByIdsSearch();
+
+  useEffect(() => {
+    if (entity?.children){
+      searchChildren(entity.children);
+    }
+  }, [entity]);
+
   return (
     <EntityContainer>
-      {entity.children.map((childEntity) => (
+      {returnedChildren && 
+      returnedChildren.map((childEntity) => (
           <MultiBtnComp
             label={childEntity.name}
             key={childEntity.name}
---
+++ src/hooks/queries/useChildrenByIdsSearch.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
+export const SEARCH_CHILDREN_BY_ID_QUERY = gql`
+  query ($ids: [ID]!) {
+    getChildrenById(ids: $ids) {
+      id
+      name
+      type
+      mainLink
+      briefDescription
+      teamsResponsible
+      properties {
+        docs
+        tags
+        technologies
+      }
+      children
+      connections {
+        audienceFacing
+        receivesDataFrom
+        givesDataTo
+      }
+      interactions {
+        isLinkUpToDate
+        comments {
+          timeStamp
+          userId
+          text
+        }
+        requestedActions {
+          timeStamp
+          typeOfAction
+          description
+          requestingUserId
+        }
+      }
+    }
+  }
+`;
+
+const useChildrenByIdsSearch = () => {
+  const [returnedChildren, setReturnedChildren] = useState("");
+
+  const [query, { loading, error, data }] = useLazyQuery(
+    SEARCH_CHILDREN_BY_ID_QUERY
+  );
+
+  const searchChildren = (ids) => {
+    query({
+      variables: { ids },
+    });
+  };
+
+  useEffect(() => {
+    // if (loading) {
+
+    // }
+    if (error) {
+      console.error("error", error)
+    }
+    if (data?.getChildrenById) {
+      setReturnedChildren(data.getChildrenById);
+    }
+  }, [data]);
+
+  return [returnedChildren, searchChildren];
+};
+
+export default useChildrenByIdsSearch;
