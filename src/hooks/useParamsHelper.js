a372629d844b0e60629667a64ce7ead94ba04305
dio-p <dionempapadop@gmail.com>
2023-07-13 17:31:43 +0100

    still more experimentation is needed

--- src/hooks/useParamsHelper.js
+++ src/hooks/useParamsHelper.js
@@ -11,33 +11,29 @@ const useParamsHelper = (paramsCustomObj, setParamsCustomObj) => {
 
   const params = Object.fromEntries([...searchParams]);
 
-  const paramNames = useMemo(() => Object.keys(paramsCustomObj), [paramsCustomObj]);
-  const paramString = useMemo(() => (paramNames.length > 2)? paramNames.join("->"): paramNames[0],[paramNames]);
+  // const paramNames = useMemo(() => Object.keys(paramsCustomObj), [paramsCustomObj]);
+  // const paramString = useMemo(() => (paramNames.length > 2)? paramNames.join("->"): paramNames[0],[paramNames]);
 
   useEffect(() => {
     if(paramsCustomObj) {
+      console.log("paramsCustomObj", paramsCustomObj);
+      const paramNames = Object.keys(paramsCustomObj);
+      const paramString = (paramNames.length >= 1)? paramNames.join("->"): paramNames[0];
       console.log("paramString", paramString);
-      setSearchParams(paramString);
-      setDisplayedEntityId((Object.values(paramsCustomObj).pop()[0]));
+      setSearchParams({ent: paramString});
+      setDisplayedEntityId((Object.values(paramsCustomObj).pop()));
     }
   }, [paramsCustomObj]);
 
   useEffect(() => {
+    // this runs also when params are added.
+    // maybe it would be better instead of deleting the last to always be getting the last
     if(params) {
       console.log("params", params);
-      // const latestParam = params.split("->").pop()[0];
-      // const paramToBeDeleted= Object.values(paramsCustomObj).pop()[0];
-      // setParamsCustomObj({...paramsCustomObj, [paramToBeDeleted]: undefined})
-      // setDisplayedEntityId(paramsCustomObj[latestParam]);
-
-      // console.log("params", params);
-      // const oldObjArray = Array.from(paramsCustomObj);
-      // console.log("oldObjArray", oldObjArray);
-      // const notNeeded = oldObjArray.pop()[0];
-      // const allParams = params.split("->");
-      // const {notNeeded, ...allParams} = paramsCustomObj
-      // setParamsCustomObj({...allParams,})
-      // setSearchParams(paramString);
+      const latestParam = params.ent.includes("->")? params.ent.split("->").pop(): params.ent;
+      const objKeyToBeDeleted= Object.keys(paramsCustomObj).pop();
+      console.log("objKeyToBeDeleted", objKeyToBeDeleted);
+      // setParamsCustomObj({...paramsCustomObj, [objKeyToBeDeleted]: undefined})
       // setDisplayedEntityId(paramsCustomObj[latestParam]);
     }
   }, [params]);
