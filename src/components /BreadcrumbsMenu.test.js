c3b275a2a5350bee00af0d7508db3c79215c087e
dio-p <dionempapadop@gmail.com>
2023-09-18 09:04:42 +0100

    finished breadcrumbsMenu

--- src/components /BreadcrumbsMenu.test.js
+++ src/components /BreadcrumbsMenu.test.js
@@ -1,36 +1,36 @@
-import { render, screen } from "@testing-library/react";
-import { MockedProvider } from "@apollo/react-testing";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import BreadcrumbsMenu from "./BreadcrumbsMenu";
-import userEvent from "@testing-library/user-event";
+import BreadcrumbsMenu from './BreadcrumbsMenu';
 
 const defaultProps = {
   paramsCustomObj: {
-    cPub: { id: "4", index: 0, name: "cPub" },
-    Authoring: { id: "5", index: 1, name: "Authoring" },
-    optimo: { id: "6", index: 2, name: "optimo" },
+    cPub: { id: '4', index: 0, name: 'cPub' },
+    Authoring: { id: '5', index: 1, name: 'Authoring' },
+    optimo: { id: '6', index: 2, name: 'optimo' },
   },
   renderChosenEntity: jest.fn(),
 };
 
-describe("Breadcrumbs Menu", () => {
+describe('Breadcrumbs Menu', () => {
   // this test can probably be improved
-  test("Should render breadcrumbs menu with correct links", () => {
+  test('Should render breadcrumbs menu with correct links', () => {
     render(<BreadcrumbsMenu {...defaultProps} />);
 
-    expect(screen.getByText("/cPub")).toBeVisible();
-    expect(screen.getByText("/Authoring")).toBeVisible();
-    expect(screen.getByText("/optimo")).toBeVisible();
+    expect(screen.getByText('/cPub')).toBeVisible();
+    expect(screen.getByText('/Authoring')).toBeVisible();
+    expect(screen.getByText('/optimo')).toBeVisible();
   });
 
-  test("Should render the right entity if link is clicked", () => {
+  test('Should render the right entity if link is clicked', () => {
     render(<BreadcrumbsMenu {...defaultProps} />);
 
-    userEvent.click(screen.getByText("/cPub"));
-    expect(defaultProps.renderChosenEntity).toHaveBeenCalledWith("cPub", "4", {
-      Authoring: { id: "5", index: 1, name: "Authoring" },
-      cPub: { id: "4", index: 0, name: "cPub" },
-      optimo: { id: "6", index: 2, name: "optimo" },
+    userEvent.click(screen.getByText('/cPub'));
+    expect(defaultProps.renderChosenEntity).toHaveBeenCalledWith('cPub', '4', {
+      Authoring: { id: '5', index: 1, name: 'Authoring' },
+      cPub: { id: '4', index: 0, name: 'cPub' },
+      optimo: { id: '6', index: 2, name: 'optimo' },
     });
   });
 });
