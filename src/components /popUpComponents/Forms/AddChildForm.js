43e4c54102b6fa82159cabdc8a0be992cbde147c
dio-p <dionempapadop@gmail.com>
2023-11-20 09:03:24 +0000

    to fix combobox, to use it on add entity

--- src/components /popUpComponents/Forms/AddChildForm.js
+++ src/components /popUpComponents/Forms/AddChildForm.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import DropDown from '../../DropDown';
+import SearchComboBox from '../../SearchComboBox';
 
 const AddChildForm = () => {
+  const [entityToGet, setEntityToGet] = useState(undefined);
   const [one] = useState('This is the AddChildForm');
   return (
     <>
       {one}
+      {/* this entity */}
+      {/* same component as to add from root with lock function to warn when trying to unlock */}
       <DropDown />
+      <SearchComboBox
+        ofType="Entity"
+        chosenValues={entityToGet}
+        onClickOption={setEntityToGet}
+      />
+      {/* new child or existing child \/ */}
       <DropDown />
     </>
   );
