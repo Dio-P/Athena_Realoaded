7555da0929483f635a2b6017934ff5e86d69b32f
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2024-07-23 21:23:04 +0300

    added a getDocSourceDetails.test file with a high level draft of the tests I need to writte. I wonder if this should not be a hook

---
+++ src/hooks/getDocSourceDetails.test.js
@@ -0,0 +1,37 @@
+// import { renderHook } from '@testing-library/react';
+
+describe('findName', () => {
+  test('should return the correct name of the doc if source is paper ', () => {
+
+  });
+  test('should return the correct name of the doc if source is confluence ', () => {
+
+  });
+  test('should return the correct name of the doc if source is github , zeplin , jira or figma ', () => {
+
+  });
+});
+
+describe('findSource', () => {
+  test('should return the correct source if link has / in it', () => {
+
+  });
+  test('should return the correct source if link has only dots in it', () => {
+
+  });
+});
+
+describe('seperateLinkParts', () => {
+  test('should seperate the link on /', () => {
+
+  });
+  test('should filter out empty strings', () => {
+
+  });
+});
+
+describe('getDocSourceDetails', () => {
+  test('happy path name and sourece are correct', () => {
+
+  });
+});
